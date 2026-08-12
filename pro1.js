ea4f2917ddc2a989bb01456110efcd19d515b16f
sapnashiraguppi <sapnashiraguppi@gmail.com>
2026-08-12 10:58:55 +0530

    correct pro1

--- pro1.js
+++ pro1.js
@@ -4,7 +4,7 @@
     resp.write(`<h1>BLDEA's Commerce BHS Arts & TGP Science College</h1>`)
     resp.write(`<h2>Started Year 1963</h2>`)
     resp.write(`<h3>Located In Jamakhandi</h3>`);
-    resp.end;
+    resp.end();
     })
     
  server.listen(3000,()=>{console.log("sever has been started")})
