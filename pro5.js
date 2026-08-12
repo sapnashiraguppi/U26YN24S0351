a08a6cf178e5779d756c74ee29141adf6f0dbd9e
sapnashiraguppi <sapnashiraguppi@gmail.com>
2026-08-12 11:12:18 +0530

    correct pro5

--- pro5.js
+++ pro5.js
@@ -1,5 +1,5 @@
 require("dotenv").config()
-const http=require("")
+const http=require("http")
 const server=http.createServer((req,resp)=>{
     resp.writeHead(200,{"content-type":"text/html"})
     resp.write(`<h1>{process.env.SERVER} has been started</h1>`)
