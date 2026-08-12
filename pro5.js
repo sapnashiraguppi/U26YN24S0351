e82fe33ef97893120c31c08fa8333ceaeff33b0e
sapnashiraguppi <sapnashiraguppi@gmail.com>
2026-08-12 11:10:44 +0530

    error pro5

--- pro5.js
+++ pro5.js
@@ -1,5 +1,5 @@
 require("dotenv").config()
-const http=require("http")
+const http=require("")
 const server=http.createServer((req,resp)=>{
     resp.writeHead(200,{"content-type":"text/html"})
     resp.write(`<h1>{process.env.SERVER} has been started</h1>`)
